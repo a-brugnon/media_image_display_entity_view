bde90ac051d0bc60dd1c5a94bb57b1fcacda7ede
arnaud-brugnon <abrugnon>
2019-12-14 09:50:38 +0100

    Image field will now be retrieve dynamically - A warning is not set if we have to media selected in the field

--- src/Plugin/Field/FieldFormatter/MediaImageDisplayFormatter.php
+++ src/Plugin/Field/FieldFormatter/MediaImageDisplayFormatter.php
@@ -4,6 +4,7 @@ namespace Drupal\media_image_display_entity_view\Plugin\Field\FieldFormatter;
 
 use Drupal\Core\Entity\Entity\EntityViewDisplay;
 use Drupal\Core\Entity\EntityDisplayRepositoryInterface;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
@@ -39,27 +40,33 @@ class MediaImageDisplayFormatter extends EntityReferenceEntityFormatter implemen
   protected $currentUser;
 
   /**
-   * The image style entity storage.
-   *
-   * @var \Drupal\image\ImageStyleStorageInterface
-   */
-  protected $imageStyleStorage;
-
-  /**
    * Entity view display.
    *
    * @var \Drupal\Core\Entity\Display\EntityViewDisplayInterface
    */
   protected $viewDisplay;
 
+
+  /**
+   * @var EntityViewDisplay
+   */
+  protected $entityViewDisplayStorage;
+
+  /**
+   * @var \Drupal\Core\Entity\EntityFieldManagerInterface
+   */
+  protected $entityFieldManager;
+
   public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition,
                               array $settings, $label, $view_mode, array $third_party_settings,
                               LoggerChannelFactoryInterface $logger_factory, EntityTypeManagerInterface $entity_type_manager,
                               EntityDisplayRepositoryInterface $entity_display_repository,
-                              AccountInterface $account, ImageStyleStorageInterface $imageStyleStorage) {
+                              AccountInterface $account, EntityTypeManagerInterface $entityTypeManager,
+                              EntityFieldManagerInterface $entityFieldManager) {
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings, $logger_factory, $entity_type_manager, $entity_display_repository);
     $this->currentUser = $account;
-    $this->imageStyleStorage = $imageStyleStorage;
+    $this->entityViewDisplayStorage = $entityTypeManager->getStorage('entity_view_display');
+    $this->entityFieldManager = $entityFieldManager;
   }
 
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
@@ -75,7 +82,8 @@ class MediaImageDisplayFormatter extends EntityReferenceEntityFormatter implemen
       $container->get('entity_type.manager'),
       $container->get('entity_display.repository'),
       $container->get('current_user'),
-      $container->get('entity_type.manager')->getStorage('image_style')
+      $container->get('entity_type.manager'),
+      $container->get('entity_field.manager')
     );
   }
 
@@ -83,17 +91,32 @@ class MediaImageDisplayFormatter extends EntityReferenceEntityFormatter implemen
   public static function defaultSettings() {
     return [
         'image_style' => '',
+        'image_field' => '',
       ] + parent::defaultSettings();
   }
 
   public function settingsForm(array $form, FormStateInterface $form_state) {
     $element = parent::settingsForm($form, $form_state);
 
+    $fieldSettings = $this->getFieldSettings();
+    $entity_type = $fieldSettings['target_type'];
+    $bundle = array_shift($fieldSettings['handler_settings']['target_bundles']);
+    $fieldsList = $this->entityFieldManager->getFieldDefinitions($entity_type, $bundle);
+
     $image_styles = image_style_options(FALSE);
     $description_link = Link::fromTextAndUrl(
       $this->t('Configure Image Styles'),
       Url::fromRoute('entity.image_style.collection')
     );
+
+    $element['image_field'] = [
+      '#title' => t('Image Field'),
+      '#type' => 'select',
+      '#default_value' => $this->getSetting('image_field'),
+      '#options' => $this->getImageFieldList($fieldsList),
+      '#required' => TRUE,
+    ];
+
     $element['image_style'] = [
       '#title' => t('Image style'),
       '#type' => 'select',
@@ -109,7 +132,16 @@ class MediaImageDisplayFormatter extends EntityReferenceEntityFormatter implemen
   }
 
   public function settingsSummary() {
-    $summary = parent::settingsSummary();
+    $summary = [];
+    $fieldSettings = $this->getFieldSettings();
+    if(count($fieldSettings['handler_settings']['target_bundles']) > 1) {
+      $summary[] = $this->t('WARNING - You have selected two media type in this field.');
+      $summary[] = $this->t('This formatter may have difficult to work.');
+    }
+
+    $summary = array_merge($summary, parent::settingsSummary());
+
+    $summary[] = $this->t('Image Field : @image_field', ['@image_field' => $this->getSetting('image_field')]);
 
     $image_styles = image_style_options(FALSE);
     // Unset possible 'No defined styles' option.
@@ -120,7 +152,7 @@ class MediaImageDisplayFormatter extends EntityReferenceEntityFormatter implemen
     if (isset($image_styles[$image_style_setting])) {
       $summary[] = $this->t('Image style: @style', ['@style' => $image_styles[$image_style_setting]]);
     }
-    else {
+  else {
       $summary[] = $this->t('Original image');
     }
 
@@ -141,11 +173,9 @@ class MediaImageDisplayFormatter extends EntityReferenceEntityFormatter implemen
 
   protected function getViewDisplay($bundle_id) {
     if (!isset($this->viewDisplay[$bundle_id])) {
-      /* todo : Make this field dynamic
-      $field_name = $this->getSetting('field_name'); */
-      $field_name = 'field_media_image';
+      $field_name = $this->getSetting('image_field');
       $entity_type_id = $this->fieldDefinition->getSetting('target_type');
-      if (($view_mode = $this->getSetting('view_mode')) && $view_display = EntityViewDisplay::load($entity_type_id . '.' . $bundle_id . '.' . $view_mode)) {
+      if (($view_mode = $this->getSetting('view_mode')) && $view_display = $this->entityViewDisplayStorage->load($entity_type_id . '.' . $bundle_id . '.' . $view_mode)) {
         /** @var \Drupal\Core\Entity\Display\EntityViewDisplayInterface $view_display */
         $components = $view_display->getComponents();
         foreach ($components as $component_name => $component) {
@@ -162,4 +192,13 @@ class MediaImageDisplayFormatter extends EntityReferenceEntityFormatter implemen
   }
 
 
+  protected function getImageFieldList(array $fieldDefinitions = []) {
+    $fields = [];
+    foreach ($fieldDefinitions as $fieldDefinition) {
+      if ($fieldDefinition->getType() == 'image' && strpos($fieldDefinition->getName(), 'field_') !== FALSE) {
+        $fields[$fieldDefinition->getName()] = $fieldDefinition->getLabel() . " (" . $fieldDefinition->getName() . ")";
+      }
+    }
+    return $fields;
+  }
 }
